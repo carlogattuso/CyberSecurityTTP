784ca119480f6a55b5a540f7ae8545aeaabb9f15
carlogattuso <carlogc46@gmail.com>
2020-03-31 17:53:22 +0200

    fixed errors

--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,13 +1,18 @@
 'use strict';
 import {Request, Response } from 'express';
 import * as bc from 'bigint-conversion';
-import {KeyPair} from "rsa";
-import {sign} from "crypto";
-import {toASCII} from "punycode";
-import {bufToHex} from "bigint-conversion";
+import {KeyPair,PublicKey} from "rsa";
 const rsa = require('rsa');
+const sha = require('object-sha');
 
 let keyPair: KeyPair;
+
+let aPubKey;
+
+let kpo: string;
+let key: string;
+
+let message;
 
 async function firstAsync() {
     return rsa.generateRandomKeys();
@@ -15,25 +20,33 @@ async function firstAsync() {
 
 firstAsync().then(data => keyPair = data);
 
-exports.getPubKey = async function (req: Request, res: Response){
-    return res.status(200).send({
-        e: bc.bigintToHex(keyPair.publicKey.e),
-        n: bc.bigintToHex(keyPair.publicKey.n)
-    });
+exports.publishKey = async function (req: Request, res: Response){
+    let b = req.body.body;
+    let signature = req.body.signature;
+
+    aPubKey = new PublicKey(bc.hexToBigint(req.body.pubKey.e),bc.hexToBigint(req.body.pubKey.n));
+    let proofDigest = bc.bigintToHex(await aPubKey.verify(bc.hexToBigint(signature)));
+    console.log(proofDigest);
+    console.log(await digest(JSON.stringify(b)));
+    let test = await digest(JSON.stringify(b));
+    if(test === proofDigest){
+        kpo = signature;
+        key = b.msg;
+        let body = { type: 4, src: 'TTP', dst: ['A','B'], msg: b.msg, timestamp: Date.now() };
+        let sign = '';
+        await digest(body)
+            .then(data => keyPair.privateKey.sign(bc.hexToBigint(data)))
+            .then(data => sign = bc.bigintToHex(data));
+
+        message = { body: body, signature: sign,
+            pubKey: { e: bc.bigintToHex(keyPair.publicKey.e), n: bc.bigintToHex(keyPair.publicKey.n) }
+        };
+        return res.status(200).send(message);
+    } else {
+        res.status(401).send({error:"Bad authentication of proof of origin"})
+    }
 };
 
-exports.sign = async function (req: Request, res: Response){
-    let message = req.body.message;
-    let signature = keyPair.privateKey.sign(bc.hexToBigint(message));
-    return res.status(200).send({
-        signature: bc.bigintToHex(signature)
-    });
-};
-
-exports.decrypt = async function (req: Request, res: Response){
-    let crypto = req.body.crypto;
-    let clearText = keyPair.privateKey.decrypt(bc.hexToBigint(crypto));
-    return res.status(200).send({
-        clearText: bc.bigintToHex(clearText)
-    });
-};
+async function digest(obj:any) {
+    return await sha.digest(obj,'SHA-256');
+}
--- routes/index.ts
+++ routes/index.ts
@@ -2,14 +2,15 @@ import { Router } from 'express';
 
 const router: Router = Router();
 
-let controller = require('./../controllers/controller');
+let controller = require('../controllers/controller');
 
 /**
  * Cyber Security Endpoint
  */
 
-router.get('/pubKey', controller.getPubKey);
-router.post('/sign', controller.sign);
-router.post('/decrypt', controller.decrypt);
+/**
+ * Non-Repudiation Service
+ */
+router.post('/nr', controller.publishKey);
 
 export default router;
